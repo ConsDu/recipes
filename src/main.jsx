707cf52d706c311a745b4e05293b556382b6a4dd
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-02-12 01:23:39 +0100

    Changed BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { Provider } from './context/recipes'
 import './scss/style.scss' // Import Bootstrap CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
